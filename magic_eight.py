24efcdc43842c6a751c2975ef8517d3872beb0a2
Shirley Lin <yuchialin0528@gmail.com>
2019-09-18 15:47:58 -0400

    adding the checking function

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,11 @@
 def question():
-    response = input("What is your question?")
+    not_quit = True
+    response = input("what is your question?")
+
+    if response != "quit":
+        while not_quit == True:
+            if response[-1] == "?":
+                not_quit = False
+            else:
+                print("I'm sorry, I can only answer questions.")
+                response = input("what is your question?")
