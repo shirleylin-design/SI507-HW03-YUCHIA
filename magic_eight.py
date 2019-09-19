c9634555ee69455c82058e7f962b0211dc1ddb32
yayingwang <yaying@umich.edu>
2019-09-19 15:53:52 -0400

    First commit from person B.

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,10 @@
+import random
+
 def question():
     response = input("What is your question?")
+
+# B - pick a random answer
+ans_list_b = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes."]
+
+ans_b = random.choice(ans_list_b)
+print(ans_b)
