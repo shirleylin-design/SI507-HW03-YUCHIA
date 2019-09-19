770192fa5b4ac19c51bb854b6a93212dce0737a5
RexSu <rexsu@0587339892.wireless.umich.net>
2019-09-19 16:21:10 -0400

    add answer C list

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,8 @@
 def question():
     response = input("What is your question?")
+
+# Question_C answer is here:
+answer_c_list =["Reply hazy, try again.","Ask  again later.","Better not tell you now.","Cannot predict now.","Concentrate and ask again.","Don't count on it.","My reply is no.","My sources say no.","Outlook not so good.","Very doubtful."]
+
+answer_c_item = random.choice(answer_c_list)
+print(answer_c_item)
