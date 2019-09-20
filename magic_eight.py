96f6129bbc09c29400082be13b404fd049d74a47
Shirley Lin <yuchialin0528@gmail.com>
2019-09-20 17:31:49 -0400

    merge two file together and it's working

--- magic_eight.py
+++ magic_eight.py
@@ -1,29 +1,30 @@
 import random
 
-def question():
-<<<<<<< HEAD
-    response = input("What is your question?")
-
 # B - pick a random answer
-ans_list_b = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes."]
 
-ans_b = random.choice(ans_list_b)
-print(ans_b)
 
-# Question_C answer is here:
-answer_c_list =["Reply hazy, try again.","Ask  again later.","Better not tell you now.","Cannot predict now.","Concentrate and ask again.","Don't count on it.","My reply is no.","My sources say no.","Outlook not so good.","Very doubtful."]
+def question():
+    ans_list_b = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes."]
+    ans_b = random.choice(ans_list_b)
 
-answer_c_item = random.choice(answer_c_list)
-print(answer_c_item)
-=======
+    # Question_C answer is here:
+    answer_c_list =["Reply hazy, try again.","Ask  again later.","Better not tell you now.","Cannot predict now.","Concentrate and ask again.","Don't count on it.","My reply is no.","My sources say no.","Outlook not so good.","Very doubtful."]
+    answer_c_item = random.choice(answer_c_list)
+# <<<<<<< HEAD
+
     not_quit = True
     response = input("what is your question?")
 
     if response != "quit":
         while not_quit == True:
             if response[-1] == "?":
+                print("This is the answer form b:" + ans_b)
+                print("This is the answer form c:" + answer_c_item)
                 not_quit = False
             else:
                 print("I'm sorry, I can only answer questions.")
                 response = input("what is your question?")
->>>>>>> check_question
+# >>>>>>> check_question
+
+
+question()
